5a7b7dd1d9952f617aa83bfd6082435d5ebf2daf
Dom Williams <dwi3774@googlemail.com>
2016-02-11 18:11:32 +0000

    Products weights are now loaded

--- src/com/godgodgodgo/Godgodgodgo.java
+++ src/com/godgodgodgo/Godgodgodgo.java
@@ -2,9 +2,6 @@ package com.godgodgodgo;
 
 public class Godgodgodgo {
     public static void main(String[] args) {
-        Loader loader = new Loader("res/busy_day.in");
-
-        System.out.println("Parameters.COL_COUNT = " + Parameters.COL_COUNT);
-        System.out.println("Parameters.DRONE_COUNT = " + Parameters.DRONE_COUNT);
+        new Loader("res/busy_day.in");
     }
 }
--- src/com/godgodgodgo/Loader.java
+++ src/com/godgodgodgo/Loader.java
@@ -1,7 +1,6 @@
 package com.godgodgodgo;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -14,11 +13,9 @@ public class Loader {
         try {
             reader = new BufferedReader(new FileReader(filePath));
             readParameters();
-
+            readProducts();
 
             reader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,5 +47,29 @@ public class Loader {
         Parameters.DRONE_COUNT = Integer.parseInt(firstLine[2]);
         Parameters.DEADLINE = Integer.parseInt(firstLine[3]);
         Parameters.MAX_LOAD = Integer.parseInt(firstLine[4]);
+    }
+
+    /**
+     * a line containing the following natural number:
+     * ○ P​     * ­ the number of different product types available in warehouses ( 1  ≤ P   ≤ 1 0000)
+     * ● a   line   containing   ​
+     * P   natural   numbers   separated   by   single   spaces   denoting   weights   of   subsequent
+     * products   types,   from   product   type   0   to   product   type   ​
+     * P   ­   1.   For   each   weight,
+     * <p>
+     * <p>
+     * 1   ≤ w eight  ≤ m aximum load of  a drone
+     */
+    private void readProducts() {
+        String[] nextLine = getNextLine();
+        int productCount = Integer.parseInt(nextLine[0]);
+
+        String[] products = getNextLine();
+        for (String product : products) {
+            int weight = Integer.parseInt(product);
+            System.out.println("weight = " + weight);
+        }
+
+
     }
 }
