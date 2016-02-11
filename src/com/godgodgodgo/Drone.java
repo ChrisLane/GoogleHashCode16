941b7885d7444223a7949000c8e8b66c49380257
Joseph Groocock <frebib@gmail.com>
2016-02-11 18:07:35 +0000

    Added Drone

---
+++ src/com/godgodgodgo/Drone.java
@@ -0,0 +1,30 @@
+public class Drone {
+    public final int capacity;
+    int payload;
+    Point destination;
+
+    public Drone(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getPayload() {
+        return payload;
+    }
+
+    public void setPayload(int payload) {
+        this.payload = payload;
+    }
+
+    public Point getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Point destination) {
+        this.destination = destination;
+    }
+
+}
