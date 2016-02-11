d2ad168b50884857c7cb8dbe3a8918387ffcf7e9
Joseph Groocock <frebib@gmail.com>
2016-02-11 18:19:19 +0000

    Added Warehouse

---
+++ src/com/godgodgodgo/Warehouse.java
@@ -0,0 +1,24 @@
+package com.godgodgodgo;
+
+import java.util.HashMap;
+
+public class Warehouse {
+    private Point location;
+    /**
+     * Key is product id, value is amount
+     */
+    private HashMap<Integer, Integer> products;
+
+    public Warehouse(Point location, HashMap<Integer, Integer> products) {
+        this.location = location;
+        this.products = products;
+    }
+
+    public HashMap<Integer, Integer> getProducts() {
+        return products;
+    }
+
+    public void setProducts(HashMap<Integer, Integer> products) {
+        this.products = products;
+    }
+}
